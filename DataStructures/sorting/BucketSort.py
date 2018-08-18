94431b7fef8acb365f28b90cebe5fb3678a60479
Naveen Pantra <naveenpantra.np@gmail.com>
2018-08-18 13:30:36 +0530

    Update BucketSort.py

--- DataStructures/sorting/BucketSort.py
+++ DataStructures/sorting/BucketSort.py
@@ -1,3 +1,5 @@
+
+
 class Head(object):
     def __init__(self, node = None, value = 0):
         self.node = node
@@ -12,9 +14,44 @@ class Node(object):
 class BucketSort(object):
     def __init__(self, arr):
         self.arr = arr
-    
-    def sort(self):
-        # Find the maximum element in the entire array
-        # By finding the maximum element in the array the find the next immidiate 100, 1000, 10000, 100000, 1000000...
-        maxi = max(self.arr)
+        self.temp = [0.0] * len(arr)
+        self.sort()
+
         
+    def insertEle(self, ele):
+        pos = int(round(ele))
+        flag = 0
+        
+        for index in range(1, len(self.temp)):
+            flag = 1
+            if self.temp[index] > ele:
+                self.temp.insert(index, ele)
+                break
+            
+        if flag == 0:
+            self.temp.append(ele)
+            
+        self.temp[pos][0] += 1
+
+        
+    def sort(self):
+        maxi = max(self.arr)
+        nums = [10, 100, 1000, 10000, 100000]
+        
+        for index in range(10):
+            self.temp.append([0])
+            
+        for index in range(1, len(nums)):
+            if maxi <= nums[index]:
+                maxi = nums[index - 1]
+                break
+            
+        for index in range(0, len(self.arr)):
+            self.arr[index] /= maxi
+        
+        for num in self.arr:
+            self.insertEle(num)
+   
+        
+bSort = BucketSort([1, 2, 3, 4, 100, 889])
+
