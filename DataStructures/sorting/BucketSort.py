000de2e4da84c6b9c87617cf83a9aa07da6aeeab
Naveen Pantra <naveenpantra.np@gmail.com>
2018-08-18 16:38:19 +0530

    Update BucketSort.py

--- DataStructures/sorting/BucketSort.py
+++ DataStructures/sorting/BucketSort.py
@@ -14,43 +14,41 @@ class Node(object):
 class BucketSort(object):
     def __init__(self, arr):
         self.arr = arr
-        self.temp = [0.0] * len(arr)
+        self.temp = []
         self.sort()
 
         
     def insertEle(self, ele):
         pos = int(round(ele))
+        print(f"Current Element: {ele} @ Position: {pos}")
         flag = 0
-        
-        for index in range(1, len(self.temp)):
+        for index in range(1, len(self.temp[pos])):
             flag = 1
-            if self.temp[index] > ele:
-                self.temp.insert(index, ele)
-                break
-            
+            if self.temp[pos][index] > ele:
+                self.temp[pos].insert(index, ele)
+                print(f"current temp: {self.temp[pos]}")
+                break     
         if flag == 0:
-            self.temp.append(ele)
-            
+            self.temp[pos].append(ele)
         self.temp[pos][0] += 1
 
         
     def sort(self):
         maxi = max(self.arr)
         nums = [10, 100, 1000, 10000, 100000]
-        
         for index in range(10):
-            self.temp.append([0])
-            
+          self.temp.append([0])
+        print(self.temp)
         for index in range(1, len(nums)):
             if maxi <= nums[index]:
                 maxi = nums[index - 1]
                 break
-            
         for index in range(0, len(self.arr)):
             self.arr[index] /= maxi
-        
         for num in self.arr:
             self.insertEle(num)
+        print(self.temp)
    
         
 bSort = BucketSort([1, 2, 3, 4, 100, 889])
+
