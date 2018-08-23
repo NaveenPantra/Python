ff07df6ce9dc6aef4307a0afe0349b94c0d63018
NaveenPantra <naveenpantra.np@gmail.com>
2018-08-23 20:16:17 +0530

    Web Scraping IMDB more titles

--- WebScraping/IMDB/01_gettingTitles.py
+++ WebScraping/IMDB/01_gettingTitles.py
@@ -15,7 +15,7 @@ class GetTitles(object):
             .
             .
     """
-    def __init__(self, title):
+    def __init__(self, title, quan):
         self.title = title
         self.url = f"https://www.imdb.com"
         self.titleResultTerms = {
@@ -24,18 +24,31 @@ class GetTitles(object):
         }
         self.imageLinks = []
         self.movieLinks = []
-        self.params = {
+        self.paramsL = {
+            f"q": title,
+            f"s": f"tt",
+        }
+        self.paramsS = {
             f"q": title,
         }
         self.result = {}
-        self.getPage()
+        if quan in ['s', 'S', 'sort', 'SORT', 'Sort']:
+            self.getPage(self.paramsS)
+        else:
+            self.getPage(self.paramsL)
 
-    def getPage(self):
-        res = request.get(f"{self.url}/find", self.params)
+    def getPage(self, params):
+        res = request.get(f"{self.url}/find", params)
         print(f"Page URL: {res.url}")
         soup = bs(res.text, 'lxml')
 
         # Get Result Title names
+        # tt = 0
+        # for section in soup.select('.findSection'):
+        #     name = section.select('a')[0]['name']
+        #     if name == 'tt':
+        #         break
+        #     tt += 1
         titleLinks = soup.select('.findSection')[0].select('.result_text')
         titleNames = []
         for title in titleLinks:
@@ -50,9 +63,11 @@ class GetTitles(object):
             self.movieLinks.append(
                 f"{self.url}{soup.select(self.titleResultTerms['titleTable'])[0].select('a')[link]['href']}")
             link += 2
+        print(f"Movies found: {len(self.movieLinks)}: {self.movieLinks}")
         index = 0
         for movieUrl in self.movieLinks:
             movie = self.getMovieDetails(movieUrl)
+            print(f"{movieUrl}: {movie}")
             self.addResult(titleNames[index], movie)
             index += 1
         self.display()
@@ -62,6 +77,11 @@ class GetTitles(object):
             print(f"{key}: {value}")
 
     def addResult(self, title, movie):
+        if title in self.result:
+            for i in range(1, 100):
+                if title + f"({i})" not in self.result:
+                    title += f"({i})"
+                    break
         self.result[title] = movie
 
     @staticmethod
@@ -88,27 +108,39 @@ class GetTitles(object):
         soup = bs(res.text, 'lxml')
 
         # Get Rating
-        movie['rating']['ratingValue'] = soup.select('.imdbRating span')[0].string
-        movie['rating']['ratingTotal'] = soup.select('.imdbRating span')[2].string
-        movie['rating']['votesTotal'] = soup.select('.imdbRating span')[3].string
-        movie['rating']['critics'] = soup.select('.imdbRating span')[-1].string
+        try:
+            movie['rating']['ratingValue'] = soup.select('.imdbRating span')[0].string
+            movie['rating']['ratingTotal'] = soup.select('.imdbRating span')[2].string
+            movie['rating']['votesTotal'] = soup.select('.imdbRating span')[3].string
+            movie['rating']['critics'] = soup.select('.imdbRating span')[-1].string
+        except:
+            pass
 
         # Movie Information
-        movie['duration'] = soup.select('.subtext time')[0].string.strip()
-        genre = soup.select('.subtext a')[:-1]
-        for typeGenre in genre:
-            movie['genre'].append(typeGenre.string)
-        movie['release'] = soup.select('.subtext a')[-1].string.strip()
+        try:
+            movie['duration'] = soup.select('.subtext time')[0].string.strip()
+            genre = soup.select('.subtext a')[:-1]
+            for typeGenre in genre:
+                movie['genre'].append(typeGenre.string)
+            movie['release'] = soup.select('.subtext a')[-1].string.strip()
+        except:
+            pass
 
         # Story line
-        movie['storyLine'] = soup.select('.summary_text')[0].string.strip()
+        try:
+            movie['storyLine'] = soup.select('.summary_text')[0].string.strip()
+        except:
+            pass
 
         # Crew
         i = 0
-        movie['crew']['director'] = soup.select('.credit_summary_item')[i].select('a')[0].string
-        i += 1
         try:
-            if soup.select('.credit_summary_item')[i].select('h4')[0].string.strip() == "Writers:":
+            movie['crew']['director'] = soup.select('.credit_summary_item')[i].select('a')[0].string
+            i += 1
+        except:
+            pass
+        try:
+            if soup.select('.credit_summary_item')[i].select('h4')[0].string.strip() in ["Writers:", "Writer:"]:
                 for writer in soup.select('.credit_summary_item')[i].select('a'):
                     movie['crew']['writer'].append(writer.string)
                 i += 1
@@ -132,7 +164,8 @@ class GetTitles(object):
 
 
 title = input("ENTER TITLE: ")
-getTitle = GetTitles('dhoom')
+quan = input("Result Length (short/long) (s/l): ")
+getTitle = GetTitles(title, quan)
 
 # self.movie = {
 #             "rating": {
